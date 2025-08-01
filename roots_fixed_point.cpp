e21aa94081bda5f779b5b8b4fa3b48a7bde567de
ahmed_ais <ahmedsalahais@live.com>
2025-08-01 11:00:51 +0300

    Create roots_fixed_point.cpp

---
+++ roots_fixed_point.cpp
@@ -0,0 +1,39 @@
+#include <iostream> // Required for input/output operations (cout, endl)
+#include <cmath>    // Required for mathematical functions (sin, sqrt, abs)
+#include <iomanip>  // Required for output formatting (setprecision, fixed)
+
+using namespace std;
+
+// you define g(x) here
+double g(double x) {
+    return 2 * sin(sqrt(x));
+}
+
+int main() {
+    // Set output precision for floating-point numbers
+    cout << fixed << setprecision(10);
+    
+    // print header
+    cout << "i\tx" << endl;
+
+    int n = 100;          // number of iterations
+    double x = 0.5;       // initial guess
+    double e = 0.0000001;  // error tolerance = 10^-7
+    
+    for (int i = 0; i < n; ++i) {
+        // print current iteration no. and value of x
+        cout << i << "\t" << x << endl;
+        
+        // calculate next value of x
+        double x_next = g(x);
+
+        // stop when conveged (even if not all iterations are used)
+        if (abs(x - x_next) < e)
+            break;
+        else
+            // keep going, update x to be what x_next have
+            x = x_next;
+    }
+
+    return 0; // Indicate successful execution
+}
